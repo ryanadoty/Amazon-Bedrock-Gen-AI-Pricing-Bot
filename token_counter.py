3c3e4c9bcc055f5e2edb139f552dc83d3de34b07
Ryan Doty <rdoty@amazon.com>
2023-11-02 15:48:53 -0400

    updates

---
+++ token_counter.py
@@ -0,0 +1,8 @@
+import tiktoken
+
+def num_tokens_from_string(string) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding("cl100k_base")
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
